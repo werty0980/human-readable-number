1af542813ccd9580b8e32d1b34f4303f79275134
werty0980 <werty0980@gmail.com>
2020-03-23 22:43:22 +0200

    feat: add resolution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function toReadable (number) {
-  
-}
+
+  module.exports = function toReadable(number) {
+    const t1 = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const t2 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const t3 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let result = '';
+    if (number == 0) {
+    result += 'zero';
+    } else {
+    let mult = Math.floor(number / 100);
+    if (mult > 0) {
+    result += t1[mult - 1] + ' hundred ';
+    number -= mult * 100;
+    }
+    if (number > 19) {
+    mult = Math.floor(number / 10);
+    result += t3[mult - 2];
+    mult = number % 10;
+    if (mult > 0) {
+    result += ' ' + t1[mult - 1];
+    }
+    } else if (number > 9) {
+    result += t2[number - 10];
+    } else if (number > 0) {
+    result += t1[number - 1];
+    }
+    }
+    return result.trim();
+    }
